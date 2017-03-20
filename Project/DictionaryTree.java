03a57ee1ef013f61e5b44012d91a6a69feac5681
momoko-w <momoko.werner@gmail.com>
2017-03-20 00:34:58 +0000

    Menu and DictionaryTree classes, first versions
    Currently not compiling!!!!!, bc other classes and methods missing from
    program as of yet (translator class also not compiling bc of this)
    Should contain all necessary methods and should generally work

--- Project/DictionaryTree.java
+++ Project/DictionaryTree.java
@@ -1,24 +1,306 @@
 
 /**
- * Write a description of class DictionaryTree here.
+ * Class that handles the binary tree methods
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Momoko Werner 
+ * @version 20/03
  */
 public class DictionaryTree
 {
-
+    //var
+    private WordNode root;
 
     /**
      * Constructor for objects of class DictionaryTree
      */
     public DictionaryTree()
     {
-
+        // initialise instance variables
+        root = null;
     }
 
-    
-    public void addToTree(int ID, String english, String french){
-        
+    /**
+     * Get the root of the tree
+     * 
+     * @return root reference to WordNode object
+     */
+    public WordNode getRoot()
+    {
+        return root;
+    }
+
+    /**
+     * Set the root of the tree to the given node
+     * 
+     * @param  newRoot  reference to a WordNode object which will be the root
+     * @return  None
+     */
+    public void setRoot(WordNode newRoot)
+    {
+        root = newRoot;
+    }
+
+    /**
+     * Add a new node to the tree which will contain the data provided ().
+     * 
+     * @param  ID   integer, ID of the word       
+     * @param  english, french  Strings holding a word and its translation respectively     
+     * @return boolean  shows whether word has been added successfully or not
+     */
+    public boolean addToTree(int ID, String english, String french)
+    {
+        // create a new node
+        WordNode newNode = new WordNode(ID, english, french); 
+        WordNode previousNode = new WordNode();
+        WordNode currentNode = new WordNode();
+        Boolean found = false;
+        previousNode = null;
+        currentNode = root;
+
+        //set new node to be the root if tree empty
+        if (root == null)
+        {
+            //tree is empty, set new node to be root
+            root = newNode;
+        }
+        else 
+        {
+            //find available free space
+            while (currentNode != null)
+            {
+                previousNode = currentNode;
+                if ((newNode.getID()) == (currentNode.getID()))
+                    return false; 
+                else if ((newNode.getID()) < (currentNode.getID()))
+                {
+                    //new Node's id smaller than current's go left
+                    currentNode = currentNode.getLeft();
+                }
+                else if ((newNode.getID()) > (currentNode.getID()))
+                {
+                    //new Node's id greater than current's go right
+                    currentNode = currentNode.getRight();
+                }
+            }
+            if ((newNode.getID()) < (previousNode.getID()))
+            {
+                //new Node's id smaller, set as left
+                previousNode.setLeft(newNode);
+            }
+            else if ((newNode.getID()) > (previousNode.getID()))
+            {
+                //new Node's id greater, set as right
+                previousNode.setRight(newNode);
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Print contents of the tree
+     * 
+     * @param None
+     * @return None
+     */
+    public void displayTree()
+    {
+        //traverse tree beginning at the root
+        traverseTree(root);
+    }
+
+    /**
+     * Traverse throught the tree inorder
+     * 
+     * @param  processNode  Node that is being processed
+     * @return None
+     */
+    public void traverseTree(WordNode processNode)
+    {
+        //
+        if (processNode != null)
+        {
+            //
+            traverseTree(processNode.getLeft());
+            System.out.println(processNode.getSummaryData());       // ?? get information from node how??
+            traverseTree(processNode.getRight());
+        }
+    }
+
+    /**
+     * Find a translation by searching for a word
+     * 
+     * @param id     holds word that is being searched for
+     * @return word  returns translation or deafult 'false' value
+     */
+    public WordNode searchTree(int id)
+    {
+        boolean found = false;
+        WordNode foundNode = null; 
+        WordNode processNode = new TreeNode();
+        //start processing at root
+        processNode = root;
+
+        while ((processNode != null) && (found == false))
+        {
+            //if IDs are the same find and return the mark of the student
+            if (id == (processNode.getID()))
+            {
+                foundNode = processNode;
+                found = true;
+                return foundNode;
+            }
+            else if (id < (processNode.getID()))
+            {
+                //given ID is smaller -> follow left branch
+                processNode = processNode.getLeft();
+            }
+            else if (id > (processNode.getID()))
+            {
+                //given ID is greater -> follow right branch
+                processNode = processNode.getRight();
+            }
+        }
+        //should return default 'false' value here
+        return foundNode;
+    }
+
+    /**
+     * Method that determines whether the tree is empty
+     * 
+     * @param  None
+     * @return Boolean - whether tree is empty or not
+     */
+    public boolean isTreeEmpty()
+    {
+        //return true if empty/root is still null
+        if (root==null)
+            return true;
+        else
+            return false;
+    }
+
+    /**
+     * Method to delete nodes from the tree
+     * 
+     * @param   id      Holds ID of the word to be deleted 
+     * @return Boolean  whether deleted successfully or not
+     */
+    public boolean removeFromTree(int id)
+    {
+        boolean found = false;
+        WordNode currentNode = new WordNode();
+        currentNode = root;
+        WordNode nodeToDelete = null;
+        WordNode previousNode = null;
+        //find node with id
+        while ((currentNode != null) && (found == false))
+        {
+            //compare IDs to see if they're the same
+            if (id == (currentNode.getID()))
+            {
+                found = true;
+            }
+            else if (id < (currentNode.getID()))
+            {
+                //save reference to previous node, given ID is smaller -> follow left branch
+                previousNode = currentNode;
+                currentNode = currentNode.getLeft();
+            }
+            else if (id > (currentNode.getID()))
+            {
+                //save reference to previous node, given ID is greater -> follow right branch
+                previousNode = currentNode;
+                currentNode = currentNode.getRight();
+            }
+        }
+        if (found == false)
+        {
+            //node with same ID wasn't found -> couldn't be deleted
+            return false;
+        }
+        else    
+        {
+            //check whether the node to be deleted has children
+            if ( ((currentNode.getRight())== null) && ((currentNode.getLeft())== null) )
+            {
+                //no children, delete reference from previous node
+                if (previousNode != null)
+                {
+                    if ((currentNode.getID()) < (previousNode.getID()))
+                        previousNode.setLeft(null);
+                    else
+                        previousNode.setRight(null);
+                }
+                else
+                    root = null;
+                //delete direct reference
+                currentNode = null;
+                return true;
+            }
+            else if ( ((currentNode.getRight())== null) && ((currentNode.getLeft())!= null) )
+            {
+                //1 child, left; set child as subtree to previous node
+                if (previousNode != null)
+                {
+                    if (((currentNode.getLeft()).getID()) < previousNode.getID())
+                    {
+                        previousNode.setLeft(currentNode.getLeft());
+                    }
+                    else 
+                        previousNode.setRight(currentNode.getLeft());
+                }
+                else
+                    root = currentNode.getLeft();
+                currentNode = null;
+                return true;
+            }
+            else if ( ((currentNode.getRight())!= null) && ((currentNode.getLeft())== null) )
+            {
+                //1 child, right; set child as subtree to previous node
+                if (previousNode != null)
+                {
+                    if (((currentNode.getRight()).getID()) < previousNode.getID())
+                    {
+                        previousNode.setLeft(currentNode.getRight());
+                    }
+                    else 
+                        previousNode.setRight(currentNode.getRight());
+                }
+                else
+                    root = currentNode.getRight();
+                currentNode = null;
+                return true;
+            }
+            else if ( ((currentNode.getRight())!= null) && ((currentNode.getLeft())!= null) )
+            {
+                //2 children
+                previousNode = null;
+                nodeToDelete = currentNode;
+                currentNode = (nodeToDelete.getLeft());
+                while (currentNode != null)
+                {
+                    //follow right branch of left subtree to find rightmost node
+                    previousNode = currentNode;
+                    currentNode = currentNode.getRight();
+                }
+                //reconnect tree correctly and remove node from tree
+                if (previousNode != null)
+                {
+                    if (previousNode.getID() < nodeToDelete.getID())
+                        previousNode.setRight(nodeToDelete.getRight());
+                    else
+                        previousNode.setLeft(nodeToDelete.getLeft());
+                }
+                else 
+                    nodeToDelete.setLeft(currentNode.getLeft());
+                if (nodeToDelete == root)
+                {
+                    root = previousNode;
+                }
+                nodeToDelete = null;
+                return true;
+            }
+        }
+        return false;
     }
 }
--- Project/Menu.java
+++ Project/Menu.java
@@ -1,33 +1,233 @@
 
 /**
- * Write a description of class Menu here.
+ * Class that allows the user to access the program's features
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Momoko Werner 
+ * @version 1.2
  */
 public class Menu
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
+    // instance variables
+    Translator translator;
+    DictionaryTree tree;
     /**
      * Constructor for objects of class Menu
      */
     public Menu()
     {
         // initialise instance variables
-        x = 0;
+        translator = new Translator();
+        tree = new DictionaryTree();
+    }
+
+    public static void main(String[] args)
+    {
+        Menu startMenu = new Menu();
+        startMenu.processUserChoices();
     }
 
     /**
-     * An example of a method - replace this comment with your own
+     * Method displaying the menu options to the user
      * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * @param  No parameters
+     * @return None
      */
-    public int sampleMethod(int y)
+    public void displayMenu()
     {
-        // put your code here
-        return x + y;
+        System.out.println("French-English Dictionary: Menu\n");
+        System.out.println("Please choose one of the options below (0-5): \n");
+        System.out.println("1. Translate from English to French.");
+        System.out.println("2. Translate from French to English.");
+        System.out.println("3. Add a word.");
+        System.out.println("4. Remove a word.");
+        System.out.println("5. Display the dictionary's contents.");
+        System.out.println("0. Exit the program\n");
+    }
+
+    /**
+     * Method allowing user to choose between menu options and processing their choice
+     * 
+     * @param No parameters
+     * @return None
+     */
+    public void processUserChoices()
+    {
+        int choice;
+
+        //translate word fr-en, en-fr and translate sentence fr-en, en-f
+        do
+        {
+            //display the menu options
+            displayMenu();
+            System.out.print("Choice: ");
+            //Prompt a choice from user
+            choice = Genio.getInteger();
+            //Compare input with menu options
+            if (choice == 1) //translate en-fr
+                translateEn();
+            else if (choice == 2) //translate fr-en
+                translateFr(); 
+            else if (choice == 3) //Add
+                addWord();
+            else if (choice == 4) //Remove
+                delWord();
+            else if (choice == 5) //Display
+                display();
+            else if (choice == 0) 
+                Menu.exit();
+            else 
+            {
+                System.out.println("\nYour chosen option does not exist.");
+                System.out.println("Please press Enter to continue and try again.");
+                // Clear terminal
+                Genio.getString();
+                System.out.print('\u000C');
+            }
+        }
+        while (choice != 0);
+    }
+
+    /**
+     * Translate from English to French     //Error catching for translation??
+     * 
+     * @param No parameters 
+     * @return None 
+     */
+    public void translateEn()
+    {
+        String enWord; 
+        String frWord;
+
+        //receive word to translate from user
+        System.out.println("Please give the English word you want to translate:"); 
+        enWord = Genio.getString();
+        frWord = translator.translateSentence(enWord, true);
+        //display the translation
+        System.out.println("\nThe translation to " +enWord+ "is: \n" +frWord+ ".");
+        System.out.println("\nPlease press Enter to continue.");
+        // Clear terminal
+        Genio.getString();
+        System.out.print('\u000C');
+    }
+
+    /**
+     * Translate from French to English //call Translator
+     * 
+     * @param No parameters 
+     * @return None 
+     */
+    public void translateFr()
+    {
+        String enWord; 
+        String frWord;
+
+        //receive word to translate from user
+        System.out.println("Please give the French word you want to translate:"); 
+        frWord = Genio.getString();
+        enWord = translator.translateSentence(frWord, false);
+        //display the translation
+        System.out.println("\nThe translation to " +frWord+ "is: \n" +enWord+ ".");
+        System.out.println("\nPlease press Enter to continue.");
+        // Clear terminal
+        Genio.getString();
+        System.out.print('\u000C');
+    }
+
+    /**
+     * Add a word given by the user     //call Translator
+     * 
+     * @param No parameters 
+     * @return None 
+     */
+    public void addWord()
+    {
+        //var
+        String frWord;
+        String enWord;
+        //prompt user for english and french words to be added
+        System.out.println("Please give the English word you want to add:");
+        enWord = Genio.getString();
+        System.out.println("\nPlease give the French translation:");
+        frWord = Genio.getString();
+        
+        //error cathcing in translator class??
+        
+        if ((translator.addWord(enWord, frWord)) == true)
+        {
+            System.out.println("\nWord added successfully!");
+        }
+        else 
+        {
+            System.out.print("\nError, word couldn't be added");
+        }
+
+        System.out.println("Please press Enter to continue.");
+        // Clear terminal
+        Genio.getString();
+        System.out.print('\u000C');    
+    }
+
+    /**
+     * Remove a word  //call Translator 
+     * 
+     * @param No parameters 
+     * @return None 
+     */
+    public void delWord()
+    {
+        //var
+        String word;
+        
+        //prompt user for english and french words to be added
+        System.out.println("Please give the word you want to remove:");
+        word = Genio.getString();
+        
+        //error cathcing in translator class??
+        if ((translator.delWord(word) == true))
+        {
+            System.out.println("\nWord removed successfully!");
+        }
+        else 
+        {
+            System.out.print("\nError, word couldn't be removed");
+        }
+
+        System.out.println("\nPlease press Enter to continue.");
+        // Clear terminal
+        Genio.getString();
+        System.out.print('\u000C');
+    }
+
+    /**
+     * Method that displays the dictionary's contents   //call tree
+     * 
+     * @param No parameters 
+     * @return None 
+     */
+    public void display()
+    {
+        if ((tree.isTreeEmpty())== false)
+        {
+            System.out.println("Dictionary:\n "); 
+            tree.displayTree();
+        }
+        else 
+            System.out.println("Error: Dictionary is empty.");
+
+        System.out.println("\nPlease press Enter to continue.");
+        // Clear terminal
+        Genio.getString();
+        System.out.print('\u000C');
+    }
+
+    /**
+     * Method to quit the menu
+     * 
+     * @param No parameters 
+     * @return None 
+     */
+    public static void exit()
+    {
+        System.out.println("\nGoodbye.");
     }
 }
