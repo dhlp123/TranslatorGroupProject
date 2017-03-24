506464cab9676133ea001ce87131c7c812c569a4
dhlp123 <dparkinson@dundee.ac.uk>
2017-03-24 16:33:27 +0000

    Fixing Compilation Errors

--- Project/Translator.java
+++ Project/Translator.java
@@ -34,40 +34,14 @@ public class Translator
 
     public String translateWord(String word, boolean isEnglish){
         String translation = "";
-<<<<<<< HEAD
+        
         if (isEnglish == true){
             translation = french.searchTree(word).getFren();
         }
         else if(isEnglish == false){
             translation = english.searchTree(word).getEng();
         }
-=======
-<<<<<<< HEAD
-            
-=======
-        {
-        /**
-        GET WORD
-        CALL METHOD searchTree() IN DictionaryTree CLASS AND PASS IN WORD
-        SET tempWord TO THE RETURNED WordNode
-        IF TRANSTYPE == 1 THEN:
-        RETURN tempWord.getFrench()
-        END IF
-        ELSE DO:
-        RETURN tempWord.getEnglish()
-        END ELSE
 
-        public String translateWord(String word, boolean isEnglish)
-        String word
-
-        
-        return translation;
-
-         **/
-    }
-
->>>>>>> origin/master
->>>>>>> origin/master
         return translation;
     }
 
